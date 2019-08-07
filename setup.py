a041fcb550f68bf7a253b77a9e903870ae0556bd
Daniel <exodaniel@gmail.com>
2019-08-07 15:55:28 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+import os
+
+
+setup(
+    name="pyniel",
+    description='Python tools for Yours Truly',
+    author='Daniel Dugas',
+    version='1.0',
+    packages=['pyniel'],
+)
