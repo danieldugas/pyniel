a76abf609276faa96c3094e80fb6f190d134bf9b
Daniel <exodaniel@gmail.com>
2020-01-13 15:58:45 +0100

    update to rosbag_end_detector

--- pyniel/ros_tools/rosbag_end_detector.py
+++ pyniel/ros_tools/rosbag_end_detector.py
@@ -3,15 +3,22 @@ from timeit import default_timer as timer
 import threading
 
 def example_rosbag_end_callback():
-    print("Rosbag end/pause detected!")
+    print("Executing code 1s (wall) after rosbag end.")
+    return
+
+def example_rosbag_start_callback():
+    print("Executing code 1s (wall) after rosbag start.")
     return
 
 class RosbagEndDetector(object):
-    def __init__(self, rosbag_end_callback=None):
+    def __init__(self, rosbag_end_callback=None, rosbag_start_callback=None):
         self.latest_rostime = None
         self.rosbag_started = False
+        if rosbag_start_callback is None:
+            rosbag_start_callback = example_rosbag_start_callback
         if rosbag_end_callback is None:
             rosbag_end_callback = example_rosbag_end_callback
+        self.rosbag_start_callback = rosbag_start_callback
         self.rosbag_end_callback = rosbag_end_callback
         # start checking in separate thread
         self.check_every_s()
@@ -21,12 +28,14 @@ class RosbagEndDetector(object):
             self.latest_rostime = rospy.Time.now()
         else:
             rostime = rospy.Time.now()
-            if rostime-self.latest_rostime <= rospy.Duration(0.05) and self.rosbag_started:
-                self.rosbag_end_callback()
-                return
-            elif not self.rosbag_started:
-                rospy.logwarn("Rosbag started, enabling end detector.")
-                self.rosbag_started = True
+            if rostime-self.latest_rostime <= rospy.Duration(0.05):
+                if self.rosbag_started:
+                    rospy.logwarn("Rosbag end/pause detected!")
+                    self.rosbag_end_callback()
+                    return
+                else:
+                    rospy.logwarn("Rosbag started, enabling end detector.")
+                    self.rosbag_started = True
             self.latest_rostime = rostime
         threading.Timer(1, self.check_every_s).start()
 
