22721d715df74c7f093a0b18229968bdd1070488
daniel <exodaniel@gmail.com>
2021-09-29 17:07:27 +0200

    interactive plot

--- pyniel/pyplot_tools/interactive.py
+++ pyniel/pyplot_tools/interactive.py
@@ -21,3 +21,42 @@ def make_legend_pickable(legend, lines):
         legenditem.set_visible(not is_visible)
         plt.gcf().canvas.draw()
     plt.connect('pick_event', on_click_legenditem)
+
+class InteractivePlot(object):
+    """
+    A simple example of a clickable plot which changes based on where it is clicked
+    """
+    def __init__(self):
+        # draggables
+        self.vcx = 0.0
+        self.vcy = -0.15
+        self.mu = 5.
+        self.rho = 1.
+        self.current_neighbor = 2
+
+        self.update_plot()
+        minvx = -2.
+        maxvx = 2.
+        minvy = -2.
+        maxvy = 2.
+        plt.axis([minvx, maxvx, minvy, maxvy])
+        def onclick(event):
+            print('%s click: button=%d, x=%d, y=%d, xdata=%f, ydata=%f' %
+                  ('double' if event.dblclick else 'single', event.button,
+                   event.x, event.y, event.xdata, event.ydata))
+            if event.button == 1:
+                self.vcx = event.xdata
+                self.vcy = event.ydata
+            elif event.button == 3:
+                delta = np.sqrt((event.xdata - self.vcx)**2 + (event.ydata - self.vcy)**2)
+                self.rho = 1. / delta / self.mu
+                print("rho={}".format(self.rho))
+        plt.figure("vc")
+        cid = plt.gcf().canvas.mpl_connect('button_press_event', onclick)
+        while True:
+            self.update_plot()
+
+    def update_plot(self):
+        plot_flow_edge_model(self.vcx, self.vcy, self.mu, self.rho, self.current_neighbor)
+        plt.pause(0.1)
+
